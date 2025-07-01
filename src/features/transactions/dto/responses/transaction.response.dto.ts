d4c8057ba65891c99d6bf1f1369730fe4d8e2adc
Edgard Alas <edgardalasu@gmail.com>
2025-07-01 13:06:22 -0600

    feat: enhance get transactions service with category filtering and date formatting

---
+++ src/features/transactions/dto/responses/transaction.response.dto.ts
@@ -0,0 +1,19 @@
+export class TransactionResponseDto {
+	transactions: {
+		id: string;
+		account: string | null;
+		category: string | null;
+		goal: string | null;
+		amount: number;
+		type: string;
+		date: Date;
+		note: string | null;
+		title: string;
+	}[];
+
+	total: number;
+
+	constructor(props: TransactionResponseDto) {
+		Object.assign(this, props);
+	}
+}
--- src/features/transactions/services/get-transactions.service.ts
+++ src/features/transactions/services/get-transactions.service.ts
@@ -1,39 +1,48 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { and, between, eq, gte, ilike, SQL } from 'drizzle-orm';
+import { and, between, eq, gte, ilike, or, SQL } from 'drizzle-orm';
+import { BaseService } from 'src/common/base-service';
 import { DatabaseService } from 'src/database/database.provider';
 import { accounts, categories, goals, transactions } from 'src/database/schema';
 import { GetTransactionsRequestDto } from 'src/features/transactions/dto/requests/get-transactions.request.dto';
+import { TransactionResponseDto } from 'src/features/transactions/dto/responses/transaction.response.dto';
+import { formatDate } from 'src/lib/format-dates';
 
 @Injectable()
-export class GetTransactionsService {
+export class GetTransactionsService
+	implements BaseService<TransactionResponseDto>
+{
 	constructor(
 		@Inject(DatabaseService) private readonly databaseService: DatabaseService,
 	) {}
 
-	async getTransactions(query: GetTransactionsRequestDto, userId: string) {
+	async execute(query: GetTransactionsRequestDto, userId: string) {
 		const filters: SQL[] = [];
 
 		if (query.startDate && query.endDate) {
 			filters.push(
 				between(
 					transactions.date,
-					new Date(query.startDate),
-					new Date(query.endDate),
+					formatDate(query.startDate).utc().toDate(),
+					formatDate(query.endDate).utc().toDate(),
 				),
 			);
 		}
 
 		if (query.startDate && !query.endDate) {
-			filters.push(gte(transactions.date, new Date(query.startDate)));
+			filters.push(
+				gte(transactions.date, formatDate(query.startDate).utc().toDate()),
+			);
 		}
 
 		if (query.endDate && !query.startDate) {
 			filters.push(
-				between(transactions.date, new Date(0), new Date(query.endDate)),
+				between(
+					transactions.date,
+					new Date(0),
+					formatDate(query.endDate).utc().toDate(),
+				),
 			);
 		}
-
-		// TODO: Implement category filter
 
 		if (query.title) {
 			filters.push(ilike(transactions.title, `%${query.title}%`));
@@ -55,17 +64,28 @@ export class GetTransactionsService {
 			.leftJoin(categories, eq(transactions.categoryId, categories.id))
 			.leftJoin(accounts, eq(transactions.accountId, accounts.id))
 			.leftJoin(goals, eq(transactions.goalId, goals.id))
-			.where(and(eq(transactions.userId, userId), ...filters));
+			.where(
+				and(
+					eq(transactions.userId, userId),
+					...filters,
+					query.category.length > 0
+						? or(
+								...query.category.map((category) =>
+									ilike(categories.name, `%${category}%`),
+								),
+							)
+						: undefined,
+				),
+			);
 
 		const total = transactionsResult.reduce(
 			(acc, transaction) => acc + transaction.amount,
 			0,
 		);
 
-		// TODO: Implement pagination and DTO
-		return {
+		return new TransactionResponseDto({
 			transactions: transactionsResult,
 			total,
-		};
+		});
 	}
 }
